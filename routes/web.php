8e8cb68b2e44003aabd61373eb6a2c4516f3e6d7
CelienFio <celien.fiorelli@ynov.com>
2023-11-23 17:13:20 +0100

    init laravel

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::prefix('/')->middleware('auth')->group(function () {
+    Route::get('', function () { return view('index'); });
+
+    Route::delete('logout', [LogoutController::class, 'destroy']);
+});
+
+
+Route::prefix('/')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])->name('register');
+    Route::post('register', [RegisterController::class, 'store']);
+    Route::get('login', [LoginController::class, 'authentificate'])->name('login');
+    Route::post('login', [LoginController::class, 'store']);
+});
